471374b0eef9795d9952ad15c18a71b2120a71e2
Bob Z Saludo <bob-zs@users.noreply.github.com>
2024-12-02 14:46:09 -0500

    fs.rmSync test

--- test/e2e.test.js
+++ test/e2e.test.js
@@ -8,32 +8,32 @@ describe('createMyApp', () => {
   let tempDir;
 
   beforeAll(() => {
-    // Ensure we are in the original directory
     if (process.cwd() !== originalDir) {
       process.chdir(originalDir);
     }
-    // Create a unique temporary directory
     const tempDirPrefix = path.join(os.tmpdir(), 'createMyApp-');
     tempDir = fs.mkdtempSync(tempDirPrefix);
   });
 
   afterAll(() => {
-    // Clean up temp directory
-    execSync(`rm -rf ${tempDir}`);
-    // Confirm the temporary directory was deleted
+    fs.rmSync(tempDir, {
+      recursive: true,
+      force: true
+    });
     expect(fs.existsSync(tempDir)).toBe(false);
   });
 
   it('should create a new app', () => {
     try {
-      // Change to the temporary directory
       process.chdir(tempDir);
 
       // Confirm we are in the temporary directory (normalize paths)
-      expect(fs.realpathSync(process.cwd())).toBe(fs.realpathSync(tempDir));
+      const currentNormalizedPath = fs.realpathSync(process.cwd());
+      const tempNormalizedPath = fs.realpathSync(tempDir);
+      expect(currentNormalizedPath).toBe(tempNormalizedPath);
 
       // Run the createMyApp command
-      execSync('npx createMyApp new-app', { stdio: 'inherit' });
+      // execSync('npx createMyApp new-app', { stdio: 'inherit' });
 
       // Verify the new app was created
       // const appPath = path.join(tempDir, 'new-app');
@@ -41,10 +41,7 @@ describe('createMyApp', () => {
       // Add more assertions as needed
 
     } finally {
-      // Ensure the original directory is restored even if the test fails
       process.chdir(originalDir);
-
-      // Confirm we are back in the original directory (normalize paths)
       expect(fs.realpathSync(process.cwd())).toBe(fs.realpathSync(originalDir));
     }
   });
